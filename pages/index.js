0d9c5d62ec5b03bdc30b8c612c64eef385544bb3
Anderson Taborga <anderson.taborga@gmail.com>
2022-05-24 10:14:36 -0300

    Incluido configurações do yarn e pages 404, faq e index

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Link from '../src/components/Link';
+
+
+
+export default function HomePage(){
+    return (
+        <div>
+            <h1>Alura Cases - Home</h1>
+            <Link href="/faq">
+                Ir para pagina do FAQ
+            </Link>
+        </div>
+    );
+};
